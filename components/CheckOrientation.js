5fa5b5a2a9c368563c1efabbc148bd9d2780ff25
ktottempudi <43048860+ktottempudi@users.noreply.github.com>
2021-08-31 16:28:13 -0400

    Check Device Orientation

--- components/CheckOrientation.js
+++ components/CheckOrientation.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Dimensions} from 'react-native';
 
 export default function CheckOrientation(){
+
+	const isPortrait = () => {
+		return Dimensions.get('window').height > Dimensions.get('window').width;
+	}
+
+	const[orientation, setOrientation] = useState(isPortrait() ? 'Portraint' : 'Landscape');
+
+	useEffect(() => {
+		const callback = () => setOrientation(isPortrait() ? 'Portrait' : 'Landscape');
+		Dimensions.addEventListener('change', callback);
+		return () => {
+			Dimensions.removeEventListener('change', callback);
+		};
+	}, []);
+
 	return(
 		<View>
-			<Text> In CheckOrientation </Text>
+			<Text> To Begin the Game please orient your phone in Landscape mode. </Text>
+			<Text> {isPortrait() ? "Portrait" : "Landscape"} </Text>
+			
 		</View>
 		);
 }
