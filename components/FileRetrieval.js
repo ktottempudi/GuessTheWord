7dbb938268889c4ad044dfeba89a2c331c31ef31
ktottempudi <43048860+ktottempudi@users.noreply.github.com>
2021-08-31 14:41:38 -0400

    Adding Famous Locations Deck

--- components/FileRetrieval.js
+++ components/FileRetrieval.js
@@ -2,6 +2,7 @@ const information = {
 	Movies: require('../Decks/Movies.json'),
 	'Harry Potter': require('../Decks/Harry Potter.json'),
 	'Game Of Thrones': require('../Decks/Game Of Thrones.json'),
+	'Famous Locations': require('../Decks/Famous Locations.json'),
 };
 
 export default information;
