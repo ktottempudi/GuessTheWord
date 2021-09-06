a89f01c857621a05e2c1bfed7206d71630cc75ac
ktottempudi <43048860+ktottempudi@users.noreply.github.com>
2021-09-06 16:37:22 -0400

    Orientation Lock on Game Screen

--- components/CheckOrientation.js
+++ components/CheckOrientation.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, Dimensions} from 'react-native';
 import Game from './Game';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function CheckOrientation({navigation}){
 
@@ -11,6 +12,7 @@ export default function CheckOrientation({navigation}){
 	const[orientation, setOrientation] = useState(isPortrait() ? 'Portraint' : 'Landscape');
 
 	useEffect(() => {
+		ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT);
 		const callback = () => setOrientation(isPortrait() ? 'Portrait' : 'Landscape');
 		Dimensions.addEventListener('change', callback);
 		return () => {
--- components/Game.js
+++ components/Game.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function Game(){
 
+	useEffect(() => {
+		ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+	});
 	return(
 		<View> 
 			<Text> In Game Screen </Text>
