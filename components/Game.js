5f38a0f24b3e1ef9aefd8abeebee4dbcbf9f8298
ktottempudi <43048860+ktottempudi@users.noreply.github.com>
2021-09-09 17:10:16 -0400

    Set Game Timer

--- components/Game.js
+++ components/Game.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 import * as ScreenOrientation from 'expo-screen-orientation';
+import Welcome from './Welcome';
 
-export default function Game(){
+export default function Game({navigation}){
 
 	const [startTimer, setStartTimer] = useState(5);
+	const [gameBegan, setGameBegan] = useState(1);
+	const [resetTimer, setResetTimer] = useState(false);
 
 	useEffect(() => {
 		ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
@@ -12,13 +15,23 @@ export default function Game(){
 			setStartTimer(lastStartTimer => {
 				if(startTimer <= 1){
 					clearInterval(interval);
+					setGameBegan(gameBegan + 1);
 					return false;
 				}
 				return lastStartTimer - 1;
 			})
 		}, 1000);
+		if(gameBegan == 2 && resetTimer == false){
+			setStartTimer(60);
+			setResetTimer(true);
+		}
+		if(gameBegan == 3){
+			ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+			navigation.navigate('Welcome');
+		}
 		return () => clearInterval(interval);
 	});
+
 	return(
 		<View> 
 			<Text> In Game Screen </Text>
