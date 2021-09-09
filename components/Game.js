14ecae86bfbb1862f89d0acbd9a1460ec3fc6844
ktottempudi <43048860+ktottempudi@users.noreply.github.com>
2021-09-09 16:04:38 -0400

    Start Timer for Game

--- components/Game.js
+++ components/Game.js
@@ -1,15 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function Game(){
 
+	const [startTimer, setStartTimer] = useState(5);
+
 	useEffect(() => {
 		ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+		let interval = setInterval(() => {
+			setStartTimer(lastStartTimer => {
+				if(startTimer <= 1){
+					clearInterval(interval);
+					return false;
+				}
+				return lastStartTimer - 1;
+			})
+		}, 1000);
+		return () => clearInterval(interval);
 	});
 	return(
 		<View> 
 			<Text> In Game Screen </Text>
+			<Text> {startTimer} </Text>
 		</View>
 		);
 }
